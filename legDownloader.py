c5b10fcd2e2fae37024f2235b307a8497507ca94
jude-ham <85447756+jude-ham@users.noreply.github.com>
2025-07-01 20:50:12 +0100

    added choice system (only uk works)

--- legDownloader.py
+++ legDownloader.py
@@ -6,9 +6,32 @@ fileName = "default.fail" # if a file is created called this, something's gone w
 fileCounterInt = 1
 fileCounterStr = ""
 legYear = 0
+legType = ""
+legTypeChoice = 0
 response = requests.get("https://www.legislation.gov.uk/ukpga/1998/46/data.pdf")
 
-legYear = int(input("What year would you like to download? "))
+choices = """1 for UK Public General Acts
+2 for Acts of the Senedd Cymru
+3 for Acts of the Scottish Parliament
+4 for Acts of the Northern Ireland Assembly
+Or, enter 0 to choose an alternative, using a custom argument (not recommended, untested)"""
+print(choices)
+legTypeChoice = int(input("What legislature do you want to use? "))
+
+if legTypeChoice == 1:
+    legType = "ukpga"
+elif legTypeChoice == 2:
+    legtype = "asc"
+    print("currently very broken and untested")
+elif legTypeChoice == 3:
+    legtype = "asp"
+    print("i've somehow broken scotland as well ffs")
+elif legTypeChoice == 4:
+    legtype = "nia"
+    print("don't event think about complaining if this doesn't work, not got round to looking at NI yet")
+else:
+    legtype = input("What arugment do you want to use? ")
+legYear = input("What year would you like to download? ")
 
 # print(response.status_code)
 # print(response.content)
@@ -17,9 +40,13 @@ legYear = int(input("What year would you like to download? "))
 
 while counter != 6:
     fileCounterStr = str(fileCounterInt)
-    response = requests.get("https://www.legislation.gov.uk/ukpga/" + str(legYear) + "/" + fileCounterStr + "/data.pdf")
+    response = requests.get("https://www.legislation.gov.uk/" + legType + "/" + legYear + "/" + fileCounterStr + "/data.pdf")
+
+    if response.status_code == 202:
+        print("File " + legType + " " + legYear + " " + fileCounterStr + " has been requested. Please try again in at least 10 seconds.")
+        exit
     
-    fileName = (str(legYear) + " Act " + fileCounterStr + ".pdf")
+    fileName = (legYear + " Act " + fileCounterStr + ".pdf")
 
     file = open(fileName, "wb")
 
